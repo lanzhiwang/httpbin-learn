563a11544f30f27147d5182f0cffb5993202c6a0
lanzhiwang <hzhilamp@163.com>
2019-11-29 12:45:08 +0800

    evokution 02

---
+++ evolution_02/README.md
@@ -0,0 +1,82 @@
+httpbin(1): HTTP Request & Response Service
+===========================================
+
+Freely hosted in [HTTP](http://httpbin.org/) &
+[HTTPS](https://httpbin.ep.io/) flavors, thanks to
+[epio](http://ep.io/).
+
+## ENDPOINTS
+
+- [`/`](http://httpbin.org/) This page.
+- [`/ip`](http://httpbin.org/ip) Returns Origin IP.
+- [`/user-agent`](http://httpbin.org/user-agent) Returns user-agent.
+- [`/headers`](http://httpbin.org/headers) Returns header dict.
+- [`/get`](http://httpbin.org/get) Returns GET data.
+- [`/post`](http://hurl.it/hurls/190ccaa90ee1ed35d34abfa4ac6ef088bc319402/d834bcdf7718c44c4184bf914ee473b6ebd8e566) Returns POST data.
+- [`/put`](http://hurl.it/hurls/18016368fa5e5eef80db935f5dae615d5858a4a4/a512d904f5da64df9627ee998c040f7874d6436a) Returns PUT data.
+- [`/patch`]() Returns PATCH data.
+- [`/delete`](http://hurl.it/hurls/6faafea5191f77172fca4cfe1505739230d5f769/bc255ffc69e04c2c8b968822c59544746bbb872c) Returns DELETE data
+- [`/gzip`](http://httpbin.org/gzip) Returns gzip-encoded data.
+- [`/status/:code`](http://httpbin.org/status/418) Returns given HTTP Status code.
+- [`/redirect/:n`](http://httpbin.org/redirect/6) 302 Redirects *n* times.
+- [`/relative-redirect/:n`](http://httpbin.org/relative-redirect/6) 302 Relative redirects *n* times.
+- [`/cookies`](http://httpbin.org/cookies) Returns cookie data.
+- [`/cookies/set/:name/:value`](http://httpbin.org/cookies/set/key/value) Sets a simple cookie.
+- [`/basic-auth/:user/:passwd`](http://httpbin.org/basic-auth/user/passwd) Challenges HTTPBasic Auth.
+- [`/hidden-basic-auth/:user/:passwd`](http://httpbin.org/hidden-basic-auth/user/passwd) 404'd BasicAuth.
+- [`/digest-auth/:qop/:user/:passwd`](http://httpbin.org/digest-auth/auth/user/passwd) Challenges HTTP Digest Auth
+
+
+## DESCRIPTION
+
+Testing an HTTP Library can become difficult sometimes. PostBin.org is fantastic
+for testing POST requests, but not much else. This exists to cover all kinds of HTTP
+scenarios. Additional endpoints are being considered (e.g. `/deflate`).
+
+All endpoint responses are JSON-encoded.
+
+
+## EXAMPLES
+
+### $ curl http://httpbin.org/ip
+
+    {"origin": "24.127.96.129"}
+
+### $ curl http://httpbin.org/user-agent
+
+    {"user-agent": "curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3"}
+
+### $ curl http://httpbin.org/get
+
+    {
+       "args": {},
+       "headers": {
+          "Accept": "*/*",
+          "Connection": "close",
+          "Content-Length": "",
+          "Content-Type": "",
+          "Host": "httpbin.org",
+          "User-Agent": "curl/7.19.7 (universal-apple-darwin10.0) libcurl/7.19.7 OpenSSL/0.9.8l zlib/1.2.3"
+       },
+       "origin": "24.127.96.129",
+       "url": "http://httpbin.org/get"
+    }
+
+### $ curl -I http://httpbin.org/status/418
+
+    HTTP/1.1 418 I'M A TEAPOT
+    Server: nginx/0.7.67
+    Date: Mon, 13 Jun 2011 04:25:38 GMT
+    Connection: close
+    x-more-info: http://tools.ietf.org/html/rfc2324
+    Content-Length: 135
+
+
+## AUTHOR
+
+A [Kenneth Reitz](http://kennethreitz.com/pages/open-projects.html)
+Project.
+
+## SEE ALSO
+
+<http://python-requests.org>
---
+++ evolution_02/httpbin/core.py
@@ -0,0 +1,232 @@
+# -*- coding: utf-8 -*-
+
+"""
+httpbin.core
+~~~~~~~~~~~~
+
+This module provides the core HttpBin experience.
+"""
+import os
+import time
+from flask import Flask, request, render_template, redirect
+from werkzeug.datastructures import WWWAuthenticate
+
+
+from . import filters
+from .helpers import get_headers, status_code, get_dict, check_basic_auth, check_digest_auth, H
+
+
+ENV_COOKIES = (
+    '_gauges_unique',
+    '_gauges_unique_year',
+    '_gauges_unique_month',
+    '_gauges_unique_day',
+    '_gauges_unique_hour',
+    '__utmz',
+    '__utma',
+    '__utmb'
+)
+
+app = Flask(__name__)
+
+
+# ------
+# Routes
+# ------
+
+
+@app.route('/')
+def view_landing_page():
+    """Generates Landing Page."""
+
+    return render_template('index.html')
+
+
+@app.route('/ip')
+@filters.json
+def view_origin():
+    """Returns Origin IP."""
+
+    return dict(origin=request.remote_addr)
+
+
+@app.route('/headers')
+@filters.json
+def view_headers():
+    """Returns HTTP HEADERS."""
+
+    return get_dict('headers')
+
+
+@app.route('/user-agent')
+@filters.json
+def view_user_agent():
+    """Returns User-Agent."""
+
+    headers = get_headers()
+
+    return {'user-agent': headers['user-agent']}
+
+
+@app.route('/get', methods=('GET',))
+@filters.json
+def view_get():
+    """Returns GET Data."""
+
+    return get_dict('url', 'args', 'headers', 'origin')
+
+
+
+@app.route('/post', methods=('POST',))
+@filters.json
+def view_post():
+    """Returns POST Data."""
+
+    return get_dict('url', 'args', 'form', 'data', 'origin', 'headers', 'files')
+
+
+@app.route('/put', methods=('PUT',))
+@filters.json
+def view_put():
+    """Returns PUT Data."""
+
+    return get_dict('url', 'args', 'form', 'data', 'origin', 'headers', 'files')
+
+
+@app.route('/patch', methods=('PATCH',))
+@filters.json
+def view_patch():
+    """Returns PATCH Data."""
+
+    return get_dict('url', 'args', 'form', 'data', 'origin', 'headers', 'files')
+
+
+
+@app.route('/delete', methods=('DELETE',))
+@filters.json
+def view_delete():
+    """Returns DETLETE Data."""
+
+    return get_dict('url', 'args', 'data', 'origin', 'headers')
+
+
+@app.route('/gzip')
+@filters.gzip
+@filters.json
+def view_gzip_encoded_content():
+    """Returns GZip-Encoded Data."""
+
+    return get_dict('origin', 'headers', method=request.method, gzipped=True)
+
+
+@app.route('/redirect/<int:n>')
+def redirect_n_times(n):
+    """301 Redirects n times."""
+
+    assert n > 0
+
+    if (n == 1):
+        return redirect('/')
+
+    return redirect('/redirect/{0}'.format(n-1))
+
+
+@app.route('/relative-redirect/<int:n>')
+def relative_redirect_n_times(n):
+    """301 Redirects n times."""
+
+    assert n > 0
+
+    response = app.make_response('')
+    response.status_code = 302
+
+    if (n == 1):
+        response.headers['Location'] = '/'
+        return response
+
+    response.headers['Location'] = '/relative-redirect/{0}'.format(n-1)
+    return response
+
+
+@app.route('/status/<int:code>')
+def view_status_code(code):
+    """Returns given status code."""
+
+    return status_code(code)
+
+
+@app.route('/cookies')
+@filters.json
+def view_cookies(hide_env=True):
+    """Returns cookie data."""
+
+    cookies = dict(request.cookies.items())
+
+    if hide_env and ('show_env' not in request.args):
+        for key in ENV_COOKIES:
+            try:
+                del cookies[key]
+            except KeyError:
+                pass
+
+    return dict(cookies=cookies)
+
+
+@app.route('/cookies/set/<name>/<value>')
+def set_cookie(name, value):
+    """Sets a cookie and redirects to cookie list."""
+
+    r = app.make_response(redirect('/cookies'))
+    r.set_cookie(key=name, value=value)
+
+    return r
+
+
+@app.route('/basic-auth/<user>/<passwd>')
+@filters.json
+def basic_auth(user='user', passwd='passwd'):
+    """Prompts the user for authorization using HTTP Basic Auth."""
+
+    if not check_basic_auth(user, passwd):
+        return status_code(401)
+    return dict(authenticated=True, user=user)
+
+
+@app.route('/hidden-basic-auth/<user>/<passwd>')
+@filters.json
+def hidden_basic_auth(user='user', passwd='passwd'):
+    """Prompts the user for authorization using HTTP Basic Auth."""
+
+    if not check_basic_auth(user, passwd):
+        return status_code(404)
+    return dict(authenticated=True, user=user)
+
+
+@app.route('/digest-auth/<qop>/<user>/<passwd>')
+@filters.json
+def digest_auth(qop=None, user='user', passwd='passwd'):
+    """Prompts the user for authorization using HTTP Digest auth"""
+    if qop not in ('auth', 'auth-int'):
+        qop = None
+    if not request.headers.get('Authorization'):
+        response = app.make_response('')
+        response.status_code = 401
+
+        nonce = H("%s:%d:%s" % (request.remote_addr,
+                                  time.time(),
+                                  os.urandom(10)))
+        opaque = H(os.urandom(10))
+
+        auth = WWWAuthenticate("digest")
+        auth.set_digest('Fake Realm', nonce, opaque=opaque,
+                        qop=('auth', 'auth-int') if qop is None else (qop, ))
+        response.headers['WWW-Authenticate'] = auth.to_header()
+        return response
+    elif not check_digest_auth(user, passwd):
+        return status_code(403)
+    return dict(authenticated=True, user=user)
+
+
+
+if __name__ == '__main__':
+    app.run()
